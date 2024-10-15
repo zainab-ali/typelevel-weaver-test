62f53cfd891cb80c0d2e3e96b9e62e43c1f8f4d9
zainab-ali <zainab@kebab-ca.se>
2024-10-15 12:15:28 +0100

    Is this better?

--- scalafix/rules/src/main/scala/fix/RenameAssertToExpect.scala
+++ scalafix/rules/src/main/scala/fix/RenameAssertToExpect.scala
@@ -1,12 +1,16 @@
 package fix
 
 import scalafix.v1._
+import scalafix.internal.config._
 import scala.meta._
 
 class RenameAssertToExpect extends SemanticRule("RenameAssertToExpect") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    Patch.replaceSymbols()
+    val assertMethod  = SymbolMatcher.normalized("weaver/Expectations.Helpers#assert.")
+    doc.tree.collect {
+      case assertMethod(tree) => Patch.renameSymbol(tree.symbol, "expect")
+    }.asPatch
   }
 
 }
